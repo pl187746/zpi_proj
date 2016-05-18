abd362d77a45197ada02e023840d147d4d535a19
Damian <edamexxx@gmail.com>
2016-05-18 15:24:05 +0200

    added first main test

---
+++ src/test/java/zpi/MainTest.java
@@ -0,0 +1,30 @@
+package zpi;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.*;
+
+public class MainTest {
+
+	private ArrayList testList;
+          
+	@BeforeClass
+	public static void start() {
+		System.out.println("@BeforeClass: executed once before all");
+	}
+	
+	@Test
+	public static void productExistTester() {
+		
+	}
+	
+	@Test
+	public static void numberOfElementsTester() {
+		
+	}
+
+	
+	
+
+}
+
