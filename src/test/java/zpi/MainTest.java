981f6c28b42a929dd1df0eacbd404435cfb7fd8a
Damian <edamexxx@gmail.com>
2016-05-18 15:44:27 +0200

    modify test

--- src/test/java/zpi/MainTest.java
+++ src/test/java/zpi/MainTest.java
@@ -2,24 +2,32 @@ package zpi;
 
 import static org.junit.Assert.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.*;
 
 public class MainTest {
 
-	private ArrayList testList;
+	private static ArrayList testList;
           
 	@BeforeClass
 	public static void start() {
 		System.out.println("@BeforeClass: executed once before all");
+		Zpi productMain = new Zpi();
 	}
 	
 	@Test
 	public static void productExistTester() {
-		
+		assertTrue(Arrays.asList(testList).contains(new String("Poland")));
 	}
 	
 	@Test
 	public static void numberOfElementsTester() {
+		
+	}
+	
+	@Test 
+	public static void taxesTester() {
 		
 	}
 
