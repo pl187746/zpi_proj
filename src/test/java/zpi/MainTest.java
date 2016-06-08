c5508f7a074b1c654ca0215c1662282e4e18e8c0
Damian <edamexxx@gmail.com>
2016-06-08 15:20:54 +0200

    fix tests

--- src/test/java/zpi/MainTest.java
+++ src/test/java/zpi/MainTest.java
@@ -6,29 +6,34 @@ import java.util.Arrays;
 
 import org.junit.*;
 
+import products.Products;
+
+
 public class MainTest {
 
-	private static ArrayList testList;
+	private static ArrayList productsList;
           
 	@BeforeClass
 	public static void start() {
 		System.out.println("@BeforeClass: executed once before all");
 		Zpi productMain = new Zpi();
+		Products Products = new Products();
+		productsList = Products.products;
 	}
 	
 	@Test
 	public static void productExistTester() {
-		assertTrue(Arrays.asList(testList).contains(new String("Poland")));
+		assertTrue(Arrays.asList(productsList).contains(new String("Cort CZ20 BK")));
 	}
 	
 	@Test
-	public static void numberOfElementsTester() {
-		
+	public static void greaterThanZeroElementsTester() {
+		assertTrue(productsList.size() > 0);
 	}
 	
 	@Test 
 	public static void taxesTester() {
-		
+
 	}
 
 	
