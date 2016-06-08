464249933a7a9df15ae6e8e2d66601351494857d
Karol <187746@edu.p.lodz.pl>
2016-06-08 15:11:49 +0200

    lista rozwijana produktow

--- src/main/java/zpi/GUI.java
+++ src/main/java/zpi/GUI.java
@@ -2,51 +2,40 @@ package zpi;
 import java.util.Scanner;
 import javax.swing.*;
 
+import products.Product;
+import products.Products;
+
 
 public class GUI extends JFrame {
 	
+	Products products = new Products();
+	JComboBox<Product> gProducts;
+	
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				GUI.start(args);
+			}
+		});
+	}
+	
+	private static void start(String[] args) {
 		GUI gui = new GUI();
 		gui.main2(args);
 	}
+	
+	private GUI() {
+		gProducts = new JComboBox<>();
+		for(Product p : products.products) {
+			gProducts.addItem(p);
+		}
+		this.add(gProducts);
+	}
 
 	public void main2(String[] args) {
-		String[] data = { "aa" };
-		JList<String> list = new JList<String>(data);
-		
-		this.add(list);
 		this.setVisible(true);
-		double price;
-		double tax;
-		int flag = 0;
-
-			showAllProducts();
-			price = chooseProduct();
-			showAllStates();
-			tax = chooseState();
-			PriceCalc calc = new PriceCalc(price, tax);
-		
-		
-		
 	}
 	
-	public void showAllProducts()
-	{
-		//String[] data = { Product.getName() };
-		//JList<String> list = new JList<String>(data);
-	}
-	public double chooseProduct()
-	{
-		return 0;
-	}
-	
-	public void showAllStates()
-	{
-		
-	}
-	public double chooseState()
-	{
-		return (double) 0.1;
-	}
 	
 }
