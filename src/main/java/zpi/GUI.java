8110e786f3b311153540a364f204864a99db614d
Karol <187746@edu.p.lodz.pl>
2016-06-21 16:52:56 +0200

    dodanie listenerów do pól tekstowych

--- src/main/java/zpi/GUI.java
+++ src/main/java/zpi/GUI.java
@@ -1,5 +1,7 @@
 package zpi;
 import java.awt.GridLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.text.DecimalFormat;
@@ -11,7 +13,7 @@ import products.Product;
 import products.Products;
 import states.State;
 import states.States;
-
+import Category.Category;
 
 public class GUI extends JFrame {
 	
@@ -20,6 +22,7 @@ public class GUI extends JFrame {
 	JComboBox<Product> gProducts;
 	JComboBox<State> gStates;
 	JFormattedTextField gProductPrice;
+	JFormattedTextField gTaxValue;
 	JFormattedTextField gGrossPrice;
 	Product currentProduct;
 	State currentState;
@@ -44,8 +47,22 @@ public class GUI extends JFrame {
 		this.setLayout(new GridLayout(2, 2, 1, 1));
 		gProductPrice = new JFormattedTextField(PRICE_FMT);
 		gProductPrice.setFocusLostBehavior(JFormattedTextField.COMMIT_OR_REVERT);
+		gTaxValue = new JFormattedTextField(PRICE_FMT);
+		gTaxValue.setFocusLostBehavior(JFormattedTextField.COMMIT_OR_REVERT);
 		gGrossPrice = new JFormattedTextField(PRICE_FMT);
 		gGrossPrice.setFocusLostBehavior(JFormattedTextField.COMMIT_OR_REVERT);
+		gProductPrice.addFocusListener(new FocusListener() {
+			@Override public void focusLost(FocusEvent e) { GUI.this.calcWithNewProductPrice(); }
+			@Override public void focusGained(FocusEvent e) { }
+		});
+		gTaxValue.addFocusListener(new FocusListener() {
+			@Override public void focusLost(FocusEvent e) { GUI.this.calcWithNewTaxValue(); }
+			@Override public void focusGained(FocusEvent e) { }
+		});
+		gGrossPrice.addFocusListener(new FocusListener() {
+			@Override public void focusLost(FocusEvent e) { GUI.this.calcWithNewGrossPrice(); }
+			@Override public void focusGained(FocusEvent e) { }
+		});
 		gStates = new JComboBox<>();
 		gProducts = new JComboBox<>();
 		for(Product p : products.products) {
@@ -54,6 +71,8 @@ public class GUI extends JFrame {
 		for(State s : states.states) {
 			gStates.addItem(s);
 		}
+		setCurrentProduct((Product)gProducts.getSelectedItem());
+		setCurrentState((State)gStates.getSelectedItem());
 		gProducts.addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
@@ -75,6 +94,7 @@ public class GUI extends JFrame {
 		this.add(gProducts);
 		this.add(gStates);
 		this.add(gProductPrice);
+		this.add(gTaxValue);
 		this.add(gGrossPrice);
 	}
 
@@ -85,10 +105,44 @@ public class GUI extends JFrame {
 	
 	private void setCurrentProduct(Product p) {
 		currentProduct = p;
+		setValues();
 	}
 	
 	private void setCurrentState(State s) {
 		currentState = s;
+		setValues();
+	}
+	
+	private double currentTax() {
+		if(currentState == null)
+			return 0.0;
+		Category category = Category.GITARA;
+		Double tax = currentState.getTax(category);
+		return (tax == null ? 0.0 : tax.doubleValue());
+	}
+	
+	private void setValues() {
+		if(currentProduct == null || currentState == null)
+			return;
+		double netPrice = currentProduct.getPrice();
+		double tax = currentTax();
+		double taxValue = netPrice * tax;
+		double grossPrice = netPrice + taxValue;
+		gProductPrice.setValue(netPrice);
+		gTaxValue.setValue(taxValue);
+		gGrossPrice.setValue(grossPrice);
+	}
+	
+	private void calcWithNewProductPrice() {
+		;
+	}
+	
+	private void calcWithNewTaxValue() {
+		;
+	}
+	
+	private void calcWithNewGrossPrice() {
+		;
 	}
 	
 }
