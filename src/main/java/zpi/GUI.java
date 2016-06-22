ae9b05f91f8dbe100cbc41326315b45d16d1484b
Karol <187746@edu.p.lodz.pl>
2016-06-22 14:36:02 +0200

    add final

--- src/main/java/zpi/GUI.java
+++ src/main/java/zpi/GUI.java
@@ -41,7 +41,7 @@ public class GUI extends JFrame {
 
 	static final Format PRICE_FMT = new DecimalFormat("0.00");
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
