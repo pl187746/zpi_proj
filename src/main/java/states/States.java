ee1d5e42ffcf5b264bc3c259ca0baa130eecdca3
MichalakJ <daran.dasaner@gmail.com>
2016-06-22 13:02:54 +0200

    dodałem Guam Montane i New Jersey

--- src/main/java/states/States.java
+++ src/main/java/states/States.java
@@ -28,6 +28,12 @@ public class States {
 				pair(Category.PRESCRIPTION_DRUG,0.00),pair(Category.NON_PRESCRIPTION_DRUG,0.0),pair(Category.CLOTHING,0.0))));
 		states.add(new State("Kansas", mapOf(pair(Category.GROCERUES, 0.0615), pair(Category.PREPARED_FOOD, 0.0615),
 				pair(Category.PRESCRIPTION_DRUG,0.0615),pair(Category.NON_PRESCRIPTION_DRUG,0.0615),pair(Category.CLOTHING,0.0615))));
+		states.add(new State("Guam", mapOf(pair(Category.GROCERUES, 0.04), pair(Category.PREPARED_FOOD, 0.04),
+				pair(Category.PRESCRIPTION_DRUG,0.04),pair(Category.NON_PRESCRIPTION_DRUG,0.04),pair(Category.CLOTHING,0.04))));
+		states.add(new State("Montana", mapOf(pair(Category.GROCERUES, 0.0), pair(Category.PREPARED_FOOD, 0.0),
+				pair(Category.PRESCRIPTION_DRUG,0.0),pair(Category.NON_PRESCRIPTION_DRUG,0.0),pair(Category.CLOTHING,0.0))));
+		states.add(new State("New Jersey", mapOf(pair(Category.GROCERUES, 0.0), pair(Category.PREPARED_FOOD, 0.07),
+				pair(Category.PRESCRIPTION_DRUG,0.0),pair(Category.NON_PRESCRIPTION_DRUG,0.07),pair(Category.CLOTHING,0.0))));
 		
 	}
 }
